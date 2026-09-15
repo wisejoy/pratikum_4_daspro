16fc2d40a64962aca4781b958b512e523bb7107a
wisejoy <alok151507@gmail.com>
2026-09-15 07:09:22 +0700

    Update rasio_akar.py

--- rasio_akar.py
+++ rasio_akar.py
@@ -1,20 +1,18 @@
+# Realisasi
+def rasio_akar(a: float, b: float, c: float) -> float:
+    diskriminan = b ** 2 - 4 * a * c
 
-def pembagian_akar(a: float, b: float, c: float) -> float:
     if a == 0:
         return -999
-    diskriminan = b ** 2 - 4 * a * c
-    if diskriminan < 0:
-        return -999
-    x1 = (-b + diskriminan ** 0.5) / (2 * a)
-    x2 = (-b - diskriminan ** 0.5) / (2 * a)
-    x_besar = max(x1, x2)
-    x_kecil = min(x1, x2)
-    if x_kecil == 0:
+    elif diskriminan < 0:
         return -999
     else:
-        return x_besar / x_kecil
+        akar1 = (-b + diskriminan ** 0.5) / (2 * a)
+        akar2 = (-b - diskriminan ** 0.5) / (2 * a)
+        akar_besar = max(akar1, akar2)
+        akar_kecil = min(akar1, akar2)
 
-print(pembagian_akar(1, -3, 2))   
-print(pembagian_akar(1, 2, 1))    
-print(pembagian_akar(1, 0, 1))   
-print(pembagian_akar(0, 2, 1))    
+        if akar_kecil == 0:
+            return -999
+        else:
+            return akar_besar / akar_kecil

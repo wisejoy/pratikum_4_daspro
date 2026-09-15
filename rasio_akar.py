e6b8e6eadc4be2d04d905d07b81b9ebe4f053f5a
wisejoy <alok151507@gmail.com>
2026-09-15 13:45:53 +0700

    update kedua

--- rasio_akar.py
+++ rasio_akar.py
@@ -1,20 +1,41 @@
+def diskriminan(a: float, b: float, c: float) -> float:
+    return b ** 2 - 4 * a * c
+
+
+def akar1(a: float, b: float, c: float) -> float:
+    return (-b + diskriminan(a, b, c) ** 0.5) / (2 * a)
+
+
+def akar2(a: float, b: float, c: float) -> float:
+    return (-b - diskriminan(a, b, c) ** 0.5) / (2 * a)
+
+
+def akar_besar(a: float, b: float, c: float) -> float:
+    if akar1(a, b, c) > akar2(a, b, c):
+        return akar1(a, b, c)
+    else:
+        return akar2(a, b, c)
+
+
+def akar_kecil(a: float, b: float, c: float) -> float:
+    if akar1(a, b, c) < akar2(a, b, c):
+        return akar1(a, b, c)
+    else:
+        return akar2(a, b, c)
+
 
 def pembagian_akar(a: float, b: float, c: float) -> float:
     if a == 0:
         return -999
-    diskriminan = b ** 2 - 4 * a * c
-    if diskriminan < 0:
+    elif diskriminan(a, b, c) < 0:
         return -999
-    x1 = (-b + diskriminan ** 0.5) / (2 * a)
-    x2 = (-b - diskriminan ** 0.5) / (2 * a)
-    x_besar = max(x1, x2)
-    x_kecil = min(x1, x2)
-    if x_kecil == 0:
+    elif akar_kecil(a, b, c) == 0:
         return -999
     else:
-        return x_besar / x_kecil
+        return akar_besar(a, b, c) / akar_kecil(a, b, c)
 
-print(pembagian_akar(1, -3, 2))   
-print(pembagian_akar(1, 2, 1))    
-print(pembagian_akar(1, 0, 1))   
-print(pembagian_akar(0, 2, 1))    
+
+print(pembagian_akar(1, -3, 2))
+print(pembagian_akar(1, 2, 1))
+print(pembagian_akar(1, 0, 1))
+print(pembagian_akar(0, 2, 1))

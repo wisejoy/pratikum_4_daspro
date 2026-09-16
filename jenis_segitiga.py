b8ea72fbaabf5f3681f124352356f7cb723d02a4
wisejoy <alok151507@gmail.com>
2026-09-16 15:54:41 +0700

    update di jenis segitiga

--- jenis_segitiga.py
+++ jenis_segitiga.py
@@ -1,13 +1,13 @@
 def jenis_segitiga(a: int, b: int, c: int) -> str:
 
     if a == b and b == c:
-        return "sama sisi"
+        return "segitiga sama sisi"
 
     elif a == b or b == c or a == c:
-        return "sama kaki"
+        return "segitiga sama kaki"
 
     else:
-        return "sembarang"
+        return "segitiga sembarang"
 
 print(jenis_segitiga(5, 5, 5))
 print(jenis_segitiga(5, 5, 8))
